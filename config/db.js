46c0548e6dc140db6fe192f605ef5ffe6a74eec8
Rahulgsr123 <rahulgsr59@gmail.com>
2025-07-25 14:54:21 +0530

    msg

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-async function connectDb(){
-try{
-    await 
-   await mongoose.connect("mongodb://localhost:27017/",{dbName:"simple_todo_db"})
-   console.log("database connected");
-   
-}catch(err){console.log(err.message);
+
+const url="mongodb+srv://rahulgsr59:UPHY7bgFPytpIKGp@cluster0.55oetam.mongodb.net/"
+async function connectDb() {
+  try {
+    await await mongoose.connect(url, {
+      dbName: "simple_todo_db",
+    });
+    console.log("database connected");
+  } catch (err) {
+    console.log(err.message);
+  }
 }
-}
 
-
-module.exports = {connectDb}
+module.exports = { connectDb };
